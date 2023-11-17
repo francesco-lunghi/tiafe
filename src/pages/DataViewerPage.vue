219b5fa217c16787438a35f3e546e97190c6b82d
francesco-lunghi <francesco-lunghi@outlook.com>
2023-11-17 12:49:22 +0100

    _c for calibrated files

--- src/pages/DataViewerPage.vue
+++ src/pages/DataViewerPage.vue
@@ -176,7 +176,8 @@ export default defineComponent({
         const url = window.URL.createObjectURL(new Blob([response.data]))
         const link = document.createElement('a')
         link.href = url
-        link.setAttribute('download', AcquisitionId + ".zip") //or any other extension
+        const cal = calibrated ? "_c" : ""
+        link.setAttribute('download', AcquisitionId + cal + ".zip") //or any other extension
         document.body.appendChild(link)
         link.click()
       })
@@ -204,7 +205,8 @@ export default defineComponent({
         const url = window.URL.createObjectURL(new Blob([response.data]))
         const link = document.createElement('a')
         link.href = url
-        link.setAttribute('download', AcquisitionId + "_" + StationId + ".zip") //or any other extension
+        const cal = calibrated ? "_c" : ""
+        link.setAttribute('download', AcquisitionId + "_" + StationId + cal + ".zip") //or any other extension
         document.body.appendChild(link)
         link.click()
       })
